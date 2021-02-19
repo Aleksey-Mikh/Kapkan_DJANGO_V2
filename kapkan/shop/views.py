76688edebbb3cf232d89d66308b2752a1dddf058
Aleksey-Mikh <Lehado67@gamil.com>
2021-02-19 14:38:04 +0300

    доработка корзины и создание основы приложения для заказа

--- kapkan/shop/views.py
+++ kapkan/shop/views.py
@@ -11,9 +11,11 @@ def index(request):
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
     products = category.product.filter(is_published=True)
+    cart_product_form = CartAddProductForm()
     context = {
         'products': products,
         'category': category,
+        'cart_product_form': cart_product_form,
     }
     return render(request, 'shop/category_product.html', context)
 
