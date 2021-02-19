92ead43c3aaf94852ecc78f05652f6f6741e59cd
Aleksey-Mikh <Lehado67@gamil.com>
2021-02-19 15:14:34 +0300

    Создана форма заказа и её обработка. Нет HTML и CSS редактрирования

--- kapkan/kapkan/urls.py
+++ kapkan/kapkan/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
+    path('orders/', include(('orders.urls', 'orders'), namespace='orders')),
     path('', include('shop.urls')),
 ]
 
--- kapkan/orders/models.py
+++ kapkan/orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from shop.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    address = models.CharField(max_length=250, verbose_name='Адрес')
+    city = models.CharField(max_length=100, verbose_name='Город')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата измения')
+    paid = models.BooleanField(default=False, verbose_name='статус оплаты')
+    comment_for_order = models.TextField(blank=True, null=True, verbose_name='Комментарий к заказу')
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
