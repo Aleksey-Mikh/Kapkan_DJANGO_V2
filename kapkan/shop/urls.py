eaf3f59c5cc5ad2ac040ef21537f8cc41064e734
Aleksey-Mikh <Lehado67@gamil.com>
2021-02-24 17:51:02 +0300

    рефакторинг views приложения shop

--- kapkan/shop/urls.py
+++ kapkan/shop/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('category/<str:slug>', category_detail, name='category'),
-    path('product/<str:slug>', product_detail, name='product_detail'),
+    path('', IndexView.as_view(), name='home'),
+    path('category/<str:slug>', CategoryDetailView.as_view(), name='category'),
+    path('product/<str:slug>', ProductDetailView.as_view(), name='product_detail'),
 ]
--- kapkan/shop/views.py
+++ kapkan/shop/views.py
@@ -1,30 +1,50 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from .models import Product, Category
 from cart.forms import CartAddProductForm
 
 
+class IndexView(ListView):
 
-def index(request):
-    return render(request, 'shop/index.html',)
+    # потом добавить на главный экран вывод категорий в 'картах', а не статичные заглушки
+    model = Category
+    template_name = 'shop/index.html'
 
-
-def category_detail(request, slug):
-    category = get_object_or_404(Category, slug=slug)
-    products = category.product.filter(is_published=True).select_related('category')
-    cart_product_form = CartAddProductForm()
-    context = {
-        'products': products,
-        'category': category,
-        'cart_product_form': cart_product_form,
-    }
-    return render(request, 'shop/category_product.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        return context
 
 
-def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug)
-    cart_product_form = CartAddProductForm()
-    context = {
-        'product': product,
-        'cart_product_form': cart_product_form,
-    }
-    return render(request, 'shop/product_detail.html', context)
+class CategoryDetailView(ListView):
+
+    model = Product
+    template_name = 'shop/category_product.html'
+    context_object_name = 'products'
+    allow_empty = False
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return category.product.filter(is_published=True).select_related('category')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_product_form = CartAddProductForm()
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        context['cart_product_form'] = cart_product_form
+        return context
+
+
+class ProductDetailView(DetailView):
+
+    model = Product
+    template_name = 'shop/product_detail.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_product_form = CartAddProductForm()
+        context['title'] = Product.objects.get(slug=self.kwargs['slug'])
+        context['cart_product_form'] = cart_product_form
+        return context
