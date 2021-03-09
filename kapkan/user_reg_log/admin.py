5d9f54f50009b5783d2039f88958d5d8f25187af
Aleksey-Mikh <lehado67@gmail.com>
2021-03-09 13:38:07 +0300

    Исправленна в админке модель пользователя. Тепрь там есть редактирование ФИО, телефона и почты пользоывателя

--- kapkan/user_reg_log/admin.py
+++ kapkan/user_reg_log/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import UserEditForm, UserRegisterForm
@@ -10,6 +9,13 @@ class CustomUserAdmin(UserAdmin):
     add_form = UserRegisterForm
     form = UserEditForm
     model = CustomUser
+    fieldsets = (
+        (None, {'fields': ('username', 'first_name', 'last_name', 'middle_name', 'email', 'phone',)}),
+        (('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
     # fieldsets = (
     #     (None, {
     #         'fields': ('username', 'first_name', 'last_name', 'email', 'phone',)
@@ -17,6 +23,5 @@ class CustomUserAdmin(UserAdmin):
     # )
     list_display = ('username', 'first_name', 'last_name', 'email', 'phone',)
     search_fields = ('title',)
-
 
 admin.site.register(CustomUser, CustomUserAdmin)
