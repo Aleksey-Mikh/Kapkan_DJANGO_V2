0a6e21d5a2e9f8c70aea3cff0d8c451966d914f7
Aleksey-Mikh <lehado67@gmail.com>
2021-03-17 13:50:57 +0300

    ubuntu res

--- kapkan/shop/urls.py
+++ kapkan/shop/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('category/<str:slug>', CategoryDetailView.as_view(), name='category'),
     path('product/<str:slug>', ProductDetailView.as_view(), name='product_detail'),
-    path('empty_cart/', empty_cart, name='empty_cart'),
+    path('empty_cart/'
+         ''
+         ''
+         ''
+         '', empty_cart, name='empty_cart'),
+
+    
 ]
