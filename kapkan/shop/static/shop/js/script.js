ff1b05f3ba5b3b18c703a228a4f8f062733a783f
Aleksey-Mikh <lehado67@gmail.com>
2021-05-05 17:28:33 +0300

    media index and video

--- kapkan/shop/static/shop/js/script.js
+++ kapkan/shop/static/shop/js/script.js
@@ -3,6 +3,20 @@ $(document).ready(function() {
   var owl = $(".owl-carousel");
   owl.owlCarousel({
     items: 3,
+    responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            800:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        },
     margin: 10,
     loop: false,
     rewind: true,
@@ -14,6 +28,7 @@ $(document).ready(function() {
     autoplayTimeout: 5000,
     smartSpeed: 2000,
     autoplayHoverPause: true,
+    responsiveBaseWidth: '#owl-example',
   });
 });
 
